7b1a440a852d0fbfc3a0260d16f42e3f75b412e8
chLi <565353780@qq.com>
2025-02-12 00:24:47 +0800

    add new test demo

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python ace_zero_run.py \
-  '/home/chli/chLi/Dataset/TARTAN/test1/input/*.jpg' \
-  '/home/chli/chLi/Dataset/TARTAN/test1/ace0/' \
+  '/home/chli/chLi/Dataset/NeRF/3vjia_person/images/*.png' \
+  './output/3vjia_person/ace0/' \
   --render_visualization True
