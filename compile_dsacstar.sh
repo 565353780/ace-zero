45822fe5946c2c7ffa63cc3c89dd5fd4b956f9a4
chli <chli@chlideMacBook-Pro.local>
2025-12-12 17:11:39 +0800

    remove source code

---
+++ compile_dsacstar.sh
@@ -0,0 +1,2 @@
+cd ../ace0/dsacstar
+python setup.py install
--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
-pip install -U torch torchvision torchaudio
+cd ..
+git clone https://github.com/nianticlabs/acezero.git ace0
 
-pip install -U pyqt5-sip pyqtchart pyqtwebengine roma timm pycolmap
-
-if [ ! -d "/usr/include/opencv2/" ]; then
-  sudo ln -s /usr/include/opencv4/opencv2 /usr/include/opencv2
-fi
-
-cd ./ace_zero/Lib/dsacstar/
-python setup.py install
+cd ace0
+conda env create -f environment.yml
